c049abebfb461bf3e1119adf5eb857fd1c69d74e
Sophia Yang <sophiayang822@gmail.com>
2025-04-10 19:28:02 -0400

    modify text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,22 @@ import React, { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState(null);
+  const [cake_data, setData1] = useState(null);
+
+  const fetchCakeData = () => {
+    fetch("https://happy-kitchen-backend-8fa23112d39e.herokuapp.com/cake_receipe/")
+      .then((response) => {
+        console.log("Did the url return the right response?")
+        
+        console.log(response)
+        return response.json()
+      })
+      .then((ascsacas) => {
+        console.log("Are we setting the data correctly?")
+        setData1(ascsacas)
+  })
+      .catch((error) => console.error("Error fetching data:", error));
+  }
 
   useEffect(() => {
     fetch("https://happy-kitchen-backend-8fa23112d39e.herokuapp.com/")
@@ -15,8 +31,37 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Happy Kitchen</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{data ? JSON.stringify(data) : "Nothing Else..."}</p>
+        <button className = "App-cake_button" onClick={fetchCakeData}>
+          Get Cake Receipe :D
+        </button>
+        <div className="App-cake_data">
+  {cake_data ? (
+    typeof cake_data === "object" ? (
+      <>
+        <h3>Ingredients:</h3>
+        <ul>
+          {Object.entries(cake_data.cake).map(([key, value], index) => (
+            <li key={index}>
+              {key}: {value}
+            </li>
+          ))}
+        </ul>
+        <h3>Instructions:</h3>
+        <ol>
+          {cake_data.instructions.map((step, index) => (
+            <li key={index}>{step}</li>
+          ))}
+        </ol>
+      </>
+    ) : (
+      <pre>{cake_data}</pre>
+    )
+  ) : (
+    "Nothing Else..."
+  )}
+</div>
         <a
           className="App-link"
           href="https://space.bilibili.com/18202105?spm_id_from=333.337.0.0"
